926202ebcc5879899ad5a6f8a0180a4c5a2308fb
AndrewDoerfler2 <andrew.doerfler@gmail.com>
2023-11-21 17:45:43 -0500

    Update welcome page

---
+++ vite-project/src/components/constants/WelcomePageText.tsx
@@ -0,0 +1,9 @@
+const WelcomePageText = {
+    title: 'Welcome to my Vite + React + TypeScript + Tailwind CSS template!',
+    description: 'This is app is a template for a Vite + React + TypeScript + Tailwind CSS app. It is a great starting point for any React project.',
+    paragraph1: 'This app is being hosted on Github Pages. You can find the source code for this app on my Github profile.',
+    contactInfo: 'Contact me at: andrew.doerfler@gmail.com or https://www.linkedin.com/in/andrewdoerfler/',
+    button: 'Get Started',
+    };
+
+export default WelcomePageText;
